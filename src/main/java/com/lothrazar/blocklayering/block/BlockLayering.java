6a6580b062ac0f8d07f2d0cf63b0810750a191ca
sam <samson.bassett@gmail.com>
2018-10-24 19:09:15 -0700

    hardness, harvest, not killed by water

--- src/main/java/com/lothrazar/blocklayering/block/BlockLayering.java
+++ src/main/java/com/lothrazar/blocklayering/block/BlockLayering.java
@@ -1,30 +1,109 @@
 package com.lothrazar.blocklayering.block;
 
 import java.util.Random;
+import javax.annotation.Nullable;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockSnow;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockLayering extends BlockSnow {
+public class BlockLayering extends Block {
 
-  private Item itemDropped;
+  public static final PropertyInteger LAYERS = PropertyInteger.create("layers", 1, 8);
+  protected static final AxisAlignedBB[] AABB = new AxisAlignedBB[] { new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.0D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.25D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.375D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.625D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.75D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.875D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D) };
 
   public BlockLayering(Material m) {
-    super();
-    this.itemDropped = Item.getItemFromBlock(this);
-    //    this.blockMaterial = m; 
+    super(m);
+    this.setHardness(1F);
+    this.setHarvestLevel("shovel", 0);
+  }
+
+
+  @Override
+  public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+    return face == EnumFacing.DOWN ? BlockFaceShape.SOLID : BlockFaceShape.UNDEFINED;
   }
 
   @Override
-  public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-    return itemDropped;
+  @Nullable
+  public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+    int i = blockState.getValue(LAYERS).intValue() - 1;
+    float f = 0.125F;
+    AxisAlignedBB axisalignedbb = blockState.getBoundingBox(worldIn, pos);
+    return new AxisAlignedBB(axisalignedbb.minX, axisalignedbb.minY, axisalignedbb.minZ, axisalignedbb.maxX, i * 0.125F, axisalignedbb.maxZ);
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+    if (side == EnumFacing.UP) {
+      return true;
+    }
+    else {
+      IBlockState iblockstate = blockAccess.getBlockState(pos.offset(side));
+      return iblockstate.getBlock() == this && iblockstate.getValue(LAYERS).intValue() >= blockState.getValue(LAYERS).intValue() ? false : super.shouldSideBeRendered(blockState, blockAccess, pos, side);
+    }
+  }
+
+  @Override
+  public IBlockState getStateFromMeta(int meta) {
+    return this.getDefaultState().withProperty(LAYERS, Integer.valueOf((meta & 7) + 1));
+  }
+
+  @Override
+  public int getMetaFromState(IBlockState state) {
+    return state.getValue(LAYERS).intValue() - 1;
+  }
+
+  @Override
+  public int quantityDropped(IBlockState state, int fortune, Random random) {
+    return (state.getValue(LAYERS)) + 1;
+  }
+
+  @Override
+  protected BlockStateContainer createBlockState() {
+    return new BlockStateContainer(this, new IProperty[] { LAYERS });
+  }
+
+  @Override
+  public boolean isOpaqueCube(IBlockState state) {
+    return false;
+  }
+
+  @Override
+  public boolean isFullCube(IBlockState state) {
+    return false;
+  }
+
+  @Override
+  public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos) {
+    return false;
+  }
+
+  @Override
+  public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    return AABB[state.getValue(LAYERS).intValue()];
+  }
+
+  @Override
+  public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+    return worldIn.getBlockState(pos).getValue(LAYERS).intValue() < 5;
+  }
+
+  @Override
+  public boolean isTopSolid(IBlockState state) {
+    return state.getValue(LAYERS).intValue() == 8;
   }
 
   @Override
